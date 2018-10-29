2a4022af606630c1d9b2bb8c76331c50e0bbe004
山尾和義 <yamao1983@gmail.com>
2018-10-29 14:50:27 +0900

    section3-24

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import axios from 'axios';
 import SearchForm from './SearchForm';
 import GeocodeResult from './GeocodeResult';
+
+const GEOCODE_ENDPOINT = 'https://maps.googleapis.com/maps/api/geocode/json';
 
 class App extends React.Component {
   constructor(props) {
@@ -8,11 +11,50 @@ class App extends React.Component {
     this.state = {};
   }
 
+  setErrorMessage(message) {
+    this.setState({
+      address: message,
+      lat: 0,
+      lng: 0,
+    });
+  }
+
   handleNameChange(name) {
     this.setState({ name });
   }
   handlePlaceSubmit(place) {
-    console.log(place);
+    axios
+      .get(GEOCODE_ENDPOINT, {
+        params: {
+          address: place,
+          key: 'APIキーを取得して設定する',
+        },
+      })
+      .then(results => {
+        const data = results.data;
+        const result = data.results[0];
+        switch (data.status) {
+          case 'OK': {
+            const location = result.geometry.location;
+            this.setState({
+              address: result.formatted_address,
+              lat: location.lat,
+              lng: location.lng,
+            });
+            break;
+          }
+          case 'ZERO_RESULT': {
+            this.setErrorMessage('結果が見つかりませんでした。');
+            break;
+          }
+          default: {
+            this.setErrorMessage('エラーが発生しました。');
+          }
+        }
+      })
+      .catch(() => {
+        this.setErrorMessage('通信に失敗しました。');
+      });
   }
 
   render() {
