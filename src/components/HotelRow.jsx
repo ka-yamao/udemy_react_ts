ccc4fd5424465b409f5d9d886b7d03ec9250fe92
山尾 和義 <kyamao@opendoor.co.jp>
2018-11-07 18:08:20 +0900

    section3-30

--- src/components/HotelRow.jsx
+++ src/components/HotelRow.jsx
@@ -4,10 +4,17 @@ import PropTypes from 'prop-types';
 const HotelRow = ({ hotel }) => (
   <tr>
     <td>
+      <img src={hotel.thumbUrl} alt={hotel.name} />
+    </td>
+    <td>
       <a href={hotel.url} target="_blank">
         {hotel.name}
       </a>
     </td>
+    <td>{hotel.price}</td>
+    <td>{hotel.reviewAverage}</td>
+    <td>{hotel.reviewCount}</td>
+    <td>{hotel.distance}</td>
   </tr>
 );
 
@@ -15,7 +22,12 @@ HotelRow.propTypes = {
   hotel: PropTypes.shape({
     name: PropTypes.string,
     url: PropTypes.string,
-  }),
+    thumbUrl: PropTypes.string,
+    price: PropTypes.string,
+    reviewAverage: PropTypes.number,
+    reviewCount: PropTypes.number,
+    distance: PropTypes.number,
+  }).isRequired,
 };
 
 export default HotelRow;
--- src/domain/HotelRepository.js
+++ src/domain/HotelRepository.js
@@ -1,4 +1,5 @@
 import Rakuten from '../lib/Rakuten';
+import geolib from 'geolib';
 
 const RAKUTEN_APP_ID = process.env.RAKUTEN_APP_ID;
 
@@ -12,10 +13,26 @@ export const searchHotelByLocation = location => {
   return Rakuten.Travel.simpleHotelSearch(params).then(result =>
     result.data.hotels.map(hotel => {
       const basicInfo = hotel.hotel[0].hotelBasicInfo;
+      const price = basicInfo.hotelMinCharge;
+      const distance = geolib.getDistance(
+        {
+          latitude: location.lat,
+          longitude: location.lng,
+        },
+        {
+          latitude: basicInfo.latitude,
+          longitude: basicInfo.longitude,
+        }
+      );
       return {
         id: basicInfo.hotelNo,
         name: basicInfo.hotelName,
         url: basicInfo.hotelInformationUrl,
+        thumbUrl: basicInfo.hotelThumbnailUrl,
+        price: price ? `${price}円` : '空室なし',
+        reviewAverage: basicInfo.reviewAverage,
+        reviewCount: basicInfo.reviewCount,
+        distance,
       };
     })
   );
