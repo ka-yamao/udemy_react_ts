9185fb3b390922cdd051ab73dad6e9378b32f02d
山尾 和義 <kyamao@opendoor.co.jp>
2018-12-11 18:14:31 +0900

    APIキーの設定

--- src/domain/Geocoder.js
+++ src/domain/Geocoder.js
@@ -7,7 +7,7 @@ export const geocode = place =>
     .get(GEOCODE_ENDPOINT, {
       params: {
         address: place,
-        key: 'APIキーを取得して設定する',
+        key: process.env.GOOGLE_MAP_API_KEY,
       },
     })
     .then(results => {
